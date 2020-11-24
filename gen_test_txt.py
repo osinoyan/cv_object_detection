5614f5e8f3c64d79228a147c02c661b53c1e2324
osinoyan <chopsticks1515@gmail.com>
2020-11-24 16:03:05 +0800

    PEP8 beautify

--- gen_test_txt.py
+++ gen_test_txt.py
@@ -1,7 +1,8 @@
 from os import listdir
 from os.path import isfile, join
 mypath = 'data/test'
-onlyfiles = ['data/test/' + f for f in sorted(listdir(mypath)) if isfile(join(mypath, f))]
+onlyfiles = ['data/test/' +
+             f for f in sorted(listdir(mypath)) if isfile(join(mypath, f))]
 print(onlyfiles[-1])
 
 darknet_data_path = '/home/apple/lee/cvHw2/darknet/data/'
